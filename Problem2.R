16ac9d31cd61b5678377c700463e959ac1c7b537
Chang-Rong Xue <changrong.xue@hotmail.com>
2024-03-12 22:13:20 +0100

    add plotting part for 2.1

--- Problem2.R
+++ Problem2.R
@@ -21,3 +21,39 @@ ggplot(data, aes(x = date, y = Yt)) + geom_line() + geom_point() +
 ggplot(data, aes(x = date, y = Xt)) + geom_line() + geom_point() +
   labs(title = "Xt", x = "Date", y = "Xt")
 
+# predict function 
+predict <- function(t) {
+  pred <- -(phi1 * data$Xt[t] + Phi1 * data$Xt[t - 12] + phi1 * Phi1 * data$Xt[t - 13]) 
+  return(pred)
+}
+
+# 2.1 
+
+start = 13
+residuals = rep(0, length(data$Xt) - start)
+for (t in start:length(data$Xt)) {
+  print(paste("t = ", t))
+  residuals[t - start] <- predict(t) - data$Xt[t - 1]
+}
+residuals <- data.frame(
+  idx = seq.int(from = start, to = start + length(residuals) - 1), 
+  vals = residuals
+)
+
+# scatter plot
+ggplot(residuals, aes(x = idx, y = vals)) + 
+  geom_point()
+
+# density histogram with a normal distribution curve
+ggplot(residuals, aes(x = vals)) + 
+  geom_histogram(aes(y = ..density..), binwidth = 0.1, colour = "black", fill = "white") +
+  stat_function(fun = dnorm, args = list(mean = 0, sd = 0.22)) +
+  labs(title = "Histogram of residuals", x = "Residuals", y = "Density")
+
+# Q-Q plot
+qqnorm(residuals$vals)
+qqline(residuals$vals)
+
+# Sign-Test
+
+
